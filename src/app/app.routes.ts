94064f930550368834208007a1bd6927be7530b6
azcakeservices <azcakeservices@azcake.az>
2024-10-22 00:16:23 +0400

    commit unused routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,20 +2,17 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from "../pages/home/home.component";
 import {LoginComponent} from "../pages/login/login.component";
 import {BranchesComponent} from "../pages/branches/branches.component";
-import {SalesComponent} from "../pages/sales/sales.component";
 import {AuthGuard} from "../guard/auth.guard";
-import {PaymentComponent} from "../pages/payment/payment.component";
-import {DetailedSalesComponent} from "../pages/detailed-sales/detailed-sales.component";
 import {SaleComponent} from "../pages/sale/sale.component";
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'sale', component: SaleComponent, canActivate: [AuthGuard] },
-  { path: 'sales', component: SalesComponent, canActivate: [AuthGuard] },
+  // { path: 'sales', component: SalesComponent, canActivate: [AuthGuard] },
   { path: 'branches', component: BranchesComponent, canActivate: [AuthGuard]},
-  { path: 'payments', component: PaymentComponent, canActivate: [AuthGuard]},
-  { path: 'detailed-sales', component: DetailedSalesComponent, canActivate: [AuthGuard]},
+  // { path: 'payments', component: PaymentComponent, canActivate: [AuthGuard]},
+  // { path: 'detailed-sales', component: DetailedSalesComponent, canActivate: [AuthGuard]},
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login'}
 ];
