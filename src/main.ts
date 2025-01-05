6c1f03f5fb7afe31b63db4d56e66b944d0d68ca4
azcakeservices <az-services@azcake.az>
2025-01-05 22:59:25 +0400

    change interception logic

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import {HTTP_INTERCEPTORS, provideHttpClient} from "@angular/common/http";
-import {AuthInterceptor} from "./guard/auth.interceptor";
-import {provideRouter, withComponentInputBinding} from "@angular/router";
-import {routes} from "./app/app.routes";
-import {ConfigService} from "./services/config.service";
-import {APP_INITIALIZER, importProvidersFrom} from "@angular/core";
-import {provideAnimations} from "@angular/platform-browser/animations";
-import {ToastrModule} from "ngx-toastr";
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
+import { AuthInterceptor } from "./guard/auth.interceptor";
+import { provideRouter, withComponentInputBinding } from "@angular/router";
+import { routes } from "./app/app.routes";
+import { ConfigService } from "./services/config.service";
+import { APP_INITIALIZER, importProvidersFrom } from "@angular/core";
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { ToastrModule } from "ngx-toastr";
 
 function intializeApp(config: ConfigService){
   return () => config.loadConfig()
@@ -16,18 +16,13 @@ function intializeApp(config: ConfigService){
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([AuthInterceptor])),
     importProvidersFrom(ToastrModule.forRoot({
       timeOut: 3000,
       preventDuplicates: true,
       positionClass: 'toast-top-right'
     })),
     ConfigService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true
-    },
     {
       provide: APP_INITIALIZER,
       useFactory: intializeApp,
