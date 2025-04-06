d69b14302b640e95199763d85350738a7307f811
azcakeservices <azcakeservices@azcake.az>
2025-04-06 21:53:20 +0400

    add unauthorized and page-not-found pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,16 +8,20 @@ import {SaleRangeComponent} from "../pages/sale-range/sale-range.component";
 import {DetailedSalesComponent} from "../pages/detailed-sales/detailed-sales.component";
 import {AdminPanelComponent} from "../pages/admin-panel/admin-panel.component";
 import {RefundComponent} from "../pages/refund/refund.component";
+import {UnauthorizedComponent} from "../pages/unauthorized/unauthorized.component";
+import {RoleGuard} from "../guard/role.guard";
+import {PageNotFoundComponent} from "../pages/page-not-found/page-not-found.component";
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'sale', component: SaleComponent, canActivate: [AuthGuard] },
-  { path: 'sale-range', component: SaleRangeComponent, canActivate: [AuthGuard] },
-  { path: 'branches', component: BranchesComponent, canActivate: [AuthGuard] },
-  { path: 'detailed-sales', component: DetailedSalesComponent, canActivate: [AuthGuard] },
-  { path: 'refund', component: RefundComponent, canActivate: [AuthGuard] },
-  { path: 'admin', component: AdminPanelComponent, canActivate: [AuthGuard] },
+  { path: 'sale', component: SaleComponent, canActivate: [AuthGuard, RoleGuard], data: { roles: ['Admin', 'SaleViewer'] } },
+  { path: 'sale-range', component: SaleRangeComponent, canActivate: [AuthGuard, RoleGuard], data: { roles: ['Admin', 'RangeSaleViewer'] } },
+  { path: 'branches', component: BranchesComponent, canActivate: [AuthGuard, RoleGuard], data: { roles: ['Admin', 'BranchViewer'] } },
+  { path: 'detailed-sales', component: DetailedSalesComponent, canActivate: [AuthGuard, RoleGuard], data: { roles: ['Admin', 'DetailedSaleViewer'] } },
+  { path: 'refund', component: RefundComponent, canActivate: [AuthGuard, RoleGuard], data: { roles: ['Admin', 'RefundViewer'] } },
+  { path: 'admin', component: AdminPanelComponent, canActivate: [AuthGuard, RoleGuard],  data: { roles: ['Admin'] }},
+  { path: 'unauthorized', component: UnauthorizedComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: '**', redirectTo: '/login'}
+  { path: '**', component: PageNotFoundComponent }
 ];
